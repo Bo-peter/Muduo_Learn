7240642399db7d71c1e1db01a5c08919b20c9ccf
Yi_Xiao <1445034376@qq.com>
2021-12-06 17:05:24 +0800

    add Buffer.h and Buffer.cc

---
+++ Buffer.cc
@@ -0,0 +1,38 @@
+#include "Buffer.h"
+#include <errno.h>
+#include <sys/uio.h>
+
+/**
+ * @brief
+ *  从fd上读取数据 Poller工作在LT模式下
+ * Buffer 缓冲区是有大小的！ 但是从fd上读数据的时候，却不知道tcp数据的大小
+ */
+
+ssize_t Buffer::readFd(int fd, int *saveErrno)
+{
+    char extrabuf[65536] = {0}; //栈上的内存空间
+    struct iovec vec[2];
+    const size_t writable = writableBytes();
+    vec[0].iov_base = begin() + writerIndex_;
+    vec[0].iov_len = writable;
+
+    vec[1].iov_base = extrabuf;
+    vec[1].iov_len = sizeof extrabuf;
+
+    const int iovcnt = (writable < sizeof extrabuf) ? 2 : 1;
+    const ssize_t n = ::readv(fd, vec, iovcnt);
+    if (n < 0)
+    {
+        *saveErrno = errno;
+    }
+    else if (n <= writable)
+    {
+        writerIndex_ += n;
+    }
+    else    //extrabuf里面存在数据
+    {
+        writerIndex_ = buffer_.size();
+        append(extrabuf,n-writable);
+    }
+    return n;
+}
